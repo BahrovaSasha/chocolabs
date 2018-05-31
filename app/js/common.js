5db5b6870950d4f3089a9b406d0f523525b8a3ff
Bahrova Aleksandra <bagrovaalexandra@gmail.com>
2018-05-31 09:56:07 +0300

    without form_no responsive

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
-
+  $(document).ready(function(){
+        $('.reviews_content').slick({
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1,
+          adaptiveHeight: true,
+          prevArrow: '<span class="arrow left_arrow"><i class="fas fa-chevron-left"></i></span>',
+          nextArrow: '<span class="arrow right_arrow"><i class="fas fa-chevron-right"></i></span>',
+          appendArrows: '.reviews'
+        });
+      });
 	// Custom JS
 
 });
