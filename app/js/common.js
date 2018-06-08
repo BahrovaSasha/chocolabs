a1ca9d9df5504f78a52249a070bde44922b770f7
Bahrova Aleksandra <bagrovaalexandra@gmail.com>
2018-06-08 13:07:53 +0300

    correct scroll menu

--- app/js/common.js
+++ app/js/common.js
@@ -12,7 +12,16 @@ $(function() {
         //open menu
         $(".burger").on("click", function (){
           $(".header_menu_wrap").toggleClass('active_nav');
+          // $(".main_header").toggleClass('fixed_header');
+      });
+      var nav = $('.main_header');
 
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 60) {
+          nav.addClass("fixed_header");
+        } else if($(this).scrollTop() <= 0 && nav.hasClass("fixed_header")) {
+          nav.removeClass("fixed_header");
+        }
       });
 	// Custom JS
 
