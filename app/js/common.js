e8f9c1c03977c27e7ffb35ffa1d6840bf7e79290
Bahrova Aleksandra <bagrovaalexandra@gmail.com>
2018-06-18 17:29:52 +0300

    fixed dogs and puppies

--- app/js/common.js
+++ app/js/common.js
@@ -47,7 +47,18 @@ $(function() {
                   slidesToShow: 2,
                 }
               },
-
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 3,
+                }
+              },
+              {
+                breakpoint: 576,
+                settings: {
+                  slidesToShow: 2,
+                }
+              },
             ]
         });
         // $('.small_carousel').slick({
